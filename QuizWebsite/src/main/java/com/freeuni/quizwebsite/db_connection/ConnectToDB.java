d93f91f5e1dc61340d64bd50fe1779546533dc7a
sskhi1 <sskhi20@freeuni.edu.ge>
2023-06-19 23:14:45 +0400

    first commit! created project

---
+++ QuizWebsite/src/main/java/com/freeuni/quizwebsite/db_connection/ConnectToDB.java
@@ -0,0 +1,34 @@
+package com.freeuni.quizwebsite.db_connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectToDB {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+
+    private static final String DB_NAME = "";  // fill in after creating sql script
+
+    private static final Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/" + DB_NAME;
+            connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void close() throws SQLException {
+        connection.close();
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+}
