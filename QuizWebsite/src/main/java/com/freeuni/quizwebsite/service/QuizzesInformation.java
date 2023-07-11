e614e593b41b3ef9f2f993f31650f9b45b169dc7
gkvash <gkvas20@freeuni.edu.ge>
2023-07-11 14:57:28 +0400

    change quiz class

--- QuizWebsite/src/main/java/com/freeuni/quizwebsite/service/QuizzesInformation.java
+++ QuizWebsite/src/main/java/com/freeuni/quizwebsite/service/QuizzesInformation.java
@@ -13,130 +13,64 @@ import java.util.List;
 public class QuizzesInformation {
     private static final Connection connection = ConnectToDB.getConnection();
 
-    public static Quiz findQuizById(int id) throws SQLException {
+    private static List<Quiz> findQuizzes(String script) throws SQLException {
         ResultSet resultSet;
+        resultSet = connection.prepareStatement(script).executeQuery();
+        List<Quiz> quizzes = new ArrayList<>();
 
-        resultSet = connection.prepareStatement("SELECT * FROM Quizes WHERE quiz_id = " + id).executeQuery();
-        if (!resultSet.next()) {
-            return null;
+        while (resultSet.next()) {
+            Quiz quiz = new Quiz(
+                    resultSet.getInt("quiz_id"),
+                    resultSet.getInt("user_id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("description"),
+                    resultSet.getBoolean("sorted"),
+                    resultSet.getBoolean("one_or_multiple"),
+                    resultSet.getBoolean("instant_feedback"),
+                    resultSet.getBoolean("practice_mode"),
+                    resultSet.getString("quiz_state"),
+                    resultSet.getInt("view_count"),
+                    resultSet.getTimestamp("creation_date")
+            );
+            quizzes.add(quiz);
         }
-        return new Quiz(resultSet.getInt("quiz_id"),
-                resultSet.getInt("user_id"),
-                resultSet.getString("name"),
-                resultSet.getString("description"),
-                resultSet.getBoolean("sorted"),
-                resultSet.getBoolean("one_or_multiple"),
-                resultSet.getBoolean("instant_feedback"),
-                resultSet.getBoolean("practice_mode"),
-                resultSet.getString("quiz_state"),
-                resultSet.getInt("view_count"),
-                resultSet.getTimestamp("creation_date"));
+
+        return quizzes;
+    }
+
+    public static Quiz findQuizById(int id) throws SQLException {
+        String script = "SELECT * FROM Quizes WHERE quiz_id = " + id;
+        List<Quiz> quizzes = findQuizzes(script);
+        if(quizzes.isEmpty()) return null;
+        return quizzes.get(0);
     }
 
 
     //this method finds all except deleted ones quizzes of user id
     public static List<Quiz> findQuizzesByUserId(int id) throws SQLException {
-        ResultSet resultSet;
-        resultSet = connection.prepareStatement("SELECT * FROM Quizes WHERE user_id = " + id
-                + " AND NOT quiz_state = \"" + QuizStates.DELETED + "\"").executeQuery();
-        List<Quiz> quizzes = new ArrayList<>();
-
-        while (resultSet.next()) {
-            Quiz quiz = new Quiz(
-                    resultSet.getInt("quiz_id"),
-                    resultSet.getInt("user_id"),
-                    resultSet.getString("name"),
-                    resultSet.getString("description"),
-                    resultSet.getBoolean("sorted"),
-                    resultSet.getBoolean("one_or_multiple"),
-                    resultSet.getBoolean("instant_feedback"),
-                    resultSet.getBoolean("practice_mode"),
-                    resultSet.getString("quiz_state"),
-                    resultSet.getInt("view_count"),
-                    resultSet.getTimestamp("creation_date")
-            );
-            quizzes.add(quiz);
-        }
-
-        return quizzes;
+        String script = "SELECT * FROM Quizes WHERE user_id = " + id
+                + " AND NOT quiz_state = \"" + QuizStates.DELETED + "\"";
+        return findQuizzes(script);
     }
 
     public static List<Quiz> findQuizzesByName(String name) throws SQLException {
-        ResultSet resultSet;
-        resultSet = connection.prepareStatement("SELECT * FROM Quizes WHERE lower(name) like \"" + "%" +
+        String script ="SELECT * FROM Quizes WHERE lower(name) like \"" + "%" +
                 name.toLowerCase() + "%" + "\""
-                + " AND NOT quiz_state = \"" + QuizStates.DELETED + "\"").executeQuery();
-        List<Quiz> quizzes = new ArrayList<>();
-
-        while (resultSet.next()) {
-            Quiz quiz = new Quiz(
-                    resultSet.getInt("quiz_id"),
-                    resultSet.getInt("user_id"),
-                    resultSet.getString("name"),
-                    resultSet.getString("description"),
-                    resultSet.getBoolean("sorted"),
-                    resultSet.getBoolean("one_or_multiple"),
-                    resultSet.getBoolean("instant_feedback"),
-                    resultSet.getBoolean("practice_mode"),
-                    resultSet.getString("quiz_state"),
-                    resultSet.getInt("view_count"),
-                    resultSet.getTimestamp("creation_date")
-            );
-            quizzes.add(quiz);
-        }
-
-        return quizzes;
+                + " AND NOT quiz_state = \"" + QuizStates.DELETED + "\"";
+        return findQuizzes(script);
     }
 
     public static List<Quiz> findQuizzesByDescription(String description) throws SQLException {
-        ResultSet resultSet;
-        resultSet = connection.prepareStatement("SELECT * FROM Quizes WHERE lower(description) like \"" + "%" +
+        String script = "SELECT * FROM Quizes WHERE lower(description) like \"" + "%" +
                 description.toLowerCase() + "%" + "\""
-                + " AND NOT quiz_state = \"" + QuizStates.DELETED + "\"").executeQuery();
-        List<Quiz> quizzes = new ArrayList<>();
-        while (resultSet.next()) {
-            Quiz quiz = new Quiz(
-                    resultSet.getInt("quiz_id"),
-                    resultSet.getInt("user_id"),
-                    resultSet.getString("name"),
-                    resultSet.getString("description"),
-                    resultSet.getBoolean("sorted"),
-                    resultSet.getBoolean("one_or_multiple"),
-                    resultSet.getBoolean("instant_feedback"),
-                    resultSet.getBoolean("practice_mode"),
-                    resultSet.getString("quiz_state"),
-                    resultSet.getInt("view_count"),
-                    resultSet.getTimestamp("creation_date")
-            );
-            quizzes.add(quiz);
-        }
-
-        return quizzes;
+                + " AND NOT quiz_state = \"" + QuizStates.DELETED + "\"";
+        return findQuizzes(script);
     }
 
     public static List<Quiz> getPublicQuizzesOrderedByViews() throws SQLException {
-        ResultSet resultSet;
-        resultSet = connection.prepareStatement("SELECT * FROM Quizes WHERE  quiz_state = \"" + QuizStates.PUBLISHED + "\" " +
-                " ORDER BY view_count DESC ").executeQuery();
-        List<Quiz> quizzes = new ArrayList<>();
-        while (resultSet.next()) {
-            Quiz quiz = new Quiz(
-                    resultSet.getInt("quiz_id"),
-                    resultSet.getInt("user_id"),
-                    resultSet.getString("name"),
-                    resultSet.getString("description"),
-                    resultSet.getBoolean("sorted"),
-                    resultSet.getBoolean("one_or_multiple"),
-                    resultSet.getBoolean("instant_feedback"),
-                    resultSet.getBoolean("practice_mode"),
-                    resultSet.getString("quiz_state"),
-                    resultSet.getInt("view_count"),
-                    resultSet.getTimestamp("creation_date")
-            );
-            quizzes.add(quiz);
-        }
-
-        return quizzes;
+        String script = "SELECT * FROM Quizes WHERE  quiz_state = \"" + QuizStates.PUBLISHED + "\" " +
+                " ORDER BY view_count DESC ";
+        return findQuizzes(script);
     }
 
 
