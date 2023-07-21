ad890e140485295c1df95a9bbd2643ae8e28c06a
Sandro Khutchua <akhuc20@freeuni.edu.ge>
2023-07-21 22:45:10 +0400

    add Question and QuestionsInformation classes

---
+++ QuizWebsite/src/main/java/com/freeuni/quizwebsite/model/QuestionType.java
@@ -0,0 +1,9 @@
+package com.freeuni.quizwebsite.model;
+
+public enum QuestionType {
+    QUESTION_RESPONSE,
+    FILL_IN_BLANK,
+    PICTURE_RESPONSE,
+    MULTIPLE_CHOICE,
+    MULTIPLE_CHOICE_MULTIPLE_ANSWER
+}
