a1e855bd6c0470cebd401440e818ad1af2cd8420
Sandro Khutchua <akhuc20@freeuni.edu.ge>
2023-07-25 23:10:19 +0400

    minor change

--- QuizWebsite/src/main/java/com/freeuni/quizwebsite/db_connection/ConnectToDB.java
+++ QuizWebsite/src/main/java/com/freeuni/quizwebsite/db_connection/ConnectToDB.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectToDB {
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Sandrojeje1";
+    private static final String PASSWORD = "";
 
     private static final String DB_NAME = "QUIZ_DB";  // fill in after creating sql script
 
