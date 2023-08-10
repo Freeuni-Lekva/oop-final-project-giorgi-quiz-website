26d5aef72f93518607880d1e665688bf77ae2f94
gkvash <gkvas20@freeuni.edu.ge>
2023-08-10 13:37:41 +0400

    minor change in evaluation servlet

--- QuizWebsite/src/main/java/com/freeuni/quizwebsite/servlet/EvaluationServlet.java
+++ QuizWebsite/src/main/java/com/freeuni/quizwebsite/servlet/EvaluationServlet.java
@@ -1,12 +1,17 @@
 package com.freeuni.quizwebsite.servlet;
 
-import jakarta.servlet.ServletException;
+import com.freeuni.quizwebsite.model.db.Question;
+import com.freeuni.quizwebsite.service.QuestionInformation;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet("/evaluation")
 public class EvaluationServlet extends HttpServlet {
@@ -14,12 +19,32 @@ public class EvaluationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         // Get the number of questions
         int numQuestions = Integer.parseInt(httpServletRequest.getParameter("numQuestions"));
+        Map<Integer,String> questionAndAnswersMap = new HashMap<>();
+        List<Integer> questionsIds = new ArrayList<>();
+        int result;
 
         // Loop through each question and print the question ID and answer text
         for (int i = 0; i < numQuestions; i++) {
-            String questionId = httpServletRequest.getParameter("questionId" + i);
+            Integer questionId = Integer.valueOf(httpServletRequest.getParameter("questionId" + i));
             String answerText = httpServletRequest.getParameter("guess" + i);
-            System.out.println("Question ID: " + questionId + ", guess: " + answerText);
+            questionAndAnswersMap.put(questionId,answerText);
+            questionsIds.add(questionId);
+        }
+        System.out.println(questionAndAnswersMap);
+
+        for (int i = 0; i <questionsIds.size() ; i++) {
+            Integer id = questionsIds.get(i);
+            Question question;
+//            List<An>
+            try {
+                question = QuestionInformation.getQuestion(id);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            if(question.getQuestionType()=="QUESTION_RESPONSE"){
+
+            }
+
         }
 
     }
