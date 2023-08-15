9fa3880c7370781bf5a09fa352f4532826b4337c
gkvash <gkvas20@freeuni.edu.ge>
2023-08-15 12:23:19 +0400

    add statistic page

---
+++ QuizWebsite/src/test/java/com/freeuni/quizwebsite/service/QuizzesInformationTest.java
@@ -0,0 +1,56 @@
+package com.freeuni.quizwebsite.service;
+
+import com.freeuni.quizwebsite.model.db.Quiz;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class QuizzesInformationTest {
+
+    @Test
+    public void findQuizById() throws SQLException {
+
+        Quiz quiz = QuizzesInformation.findQuizById(1);
+        assert quiz != null;
+
+        assertEquals(quiz.getName(),"Cars quiz");
+        assertEquals(quiz.getUserId(),1);
+    }
+
+    @Test
+    public void findQuizzesByUserId() throws SQLException {
+        List<Quiz> quizzes = QuizzesInformation.findQuizzesByUserId(1);
+        assert quizzes !=null;
+        assertEquals(3,quizzes.size());
+    }
+
+
+    @Test
+    public void findQuizzesByName() throws SQLException {
+        List<Quiz> quizzes1 = QuizzesInformation.findQuizzesByName("math");
+        List<Quiz> quizzes2 = QuizzesInformation.findQuizzesByName("car");
+        List<Quiz> quizzes3 = QuizzesInformation.findQuizzesByName("");
+        assertEquals(0,quizzes1.size());
+        assertEquals(1,quizzes2.size());
+    }
+
+    @Test
+    public void findQuizzesByDescription() throws SQLException {
+        List<Quiz> quizzes1 = QuizzesInformation.findQuizzesByDescription("math");
+        List<Quiz> quizzes2 = QuizzesInformation.findQuizzesByDescription("education");
+        List<Quiz> quizzes3 = QuizzesInformation.findQuizzesByDescription("");
+        assertEquals(0,quizzes1.size());
+        assertEquals(1,quizzes2.size());
+    }
+
+    @Test
+    public void getPublicQuizzesByViews() throws SQLException {
+        List<Quiz> quizzes = QuizzesInformation.getPublicQuizzesOrderedByViews();
+        if(quizzes.size()>=2) {
+            assertTrue(quizzes.get(0).getViewCount()>=quizzes.get(1).getViewCount());
+        }
+    }
+}
