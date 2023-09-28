fa94526d8b61b554e903a80cb3818db9f39bd871
sskhi1 <sskhi20@freeuni.edu.ge>
2023-09-28 22:55:08 +0400

    minor fix

--- QuizWebsite/src/main/java/com/freeuni/quizwebsite/service/AdminInformation.java
+++ QuizWebsite/src/main/java/com/freeuni/quizwebsite/service/AdminInformation.java
@@ -9,9 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AdminInformation {
-
     private static final Connection connection = ConnectToDB.getConnection();
-
     public static int userAmount() throws SQLException {
         String script = "SELECT count(*) as cnt FROM USERS";
         ResultSet resultSet = null;
@@ -19,6 +17,27 @@ public class AdminInformation {
 
         try {
             resultSet = connection.prepareStatement(script).executeQuery();
+
+            if (resultSet.next()) {
+                result = resultSet.getInt("cnt");
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+
+        return result;
+    }
+
+    public static int usersAddedToday() throws SQLException {
+        String script = "SELECT count(*) as cnt FROM USERS WHERE DAY(creation_date) = ?";
+        ResultSet resultSet = null;
+        int result = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(script);
+            preparedStatement.setInt(1, LocalDate.now().getDayOfMonth());
+            resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
                 result = resultSet.getInt("cnt");
@@ -67,6 +86,118 @@ public class AdminInformation {
 
     public static int quizAmount() throws SQLException {
         String script = "SELECT count(*) as cnt FROM Quizes";
+        ResultSet resultSet = null;
+        int result = 0;
+
+        try {
+            resultSet = connection.prepareStatement(script).executeQuery();
+
+            if (resultSet.next()) {
+                result = resultSet.getInt("cnt");
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+
+        return result;
+    }
+
+    public static Map<LocalDate, Integer> quizzesAddedLastSevenDays() throws SQLException {
+        String script = "SELECT DATE(creation_date) as creation_date, count(*) as cnt FROM Quizes " +
+                "WHERE creation_date >= ? " +
+                "GROUP BY DATE(creation_date)";
+        ResultSet resultSet = null;
+        Map<LocalDate, Integer> resultMap = new HashMap<>();
+
+        // Initialize the resultMap with 0 counts for the last seven days
+        LocalDate currentDate = LocalDate.now();
+        for (int i = 0; i < 7; i++) {
+            resultMap.put(currentDate.minus(i, ChronoUnit.DAYS), 0);
+        }
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(script);
+            LocalDate lastSevenDays = LocalDate.now().minus(6, ChronoUnit.DAYS);
+            preparedStatement.setDate(1, Date.valueOf(lastSevenDays));
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                LocalDate date = resultSet.getDate("creation_date").toLocalDate();
+                int count = resultSet.getInt("cnt");
+                resultMap.put(date, count);
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+
+        return resultMap;
+    }
+
+    public static int totalViews() throws SQLException {
+        String script = "SELECT sum(view_count) as cnt FROM Quizes";
+        ResultSet resultSet = null;
+        int result = 0;
+
+        try {
+            resultSet = connection.prepareStatement(script).executeQuery();
+
+            if (resultSet.next()) {
+                result = resultSet.getInt("cnt");
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+
+        return result;
+    }
+
+    public static int totalChallenges() throws SQLException {
+        String script = "SELECT count(*) as cnt FROM Challenges";
+        ResultSet resultSet = null;
+        int result = 0;
+
+        try {
+            resultSet = connection.prepareStatement(script).executeQuery();
+
+            if (resultSet.next()) {
+                result = resultSet.getInt("cnt");
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+
+        return result;
+    }
+
+    public static int totalMails() throws SQLException {
+        String script = "SELECT count(*) as cnt FROM note_mail";
+        ResultSet resultSet = null;
+        int result = 0;
+        try {
+            resultSet = connection.prepareStatement(script).executeQuery();
+
+            if (resultSet.next()) {
+                result = resultSet.getInt("cnt");
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+
+        return result;
+    }
+
+    public static int totalQuizTake() throws SQLException {
+        String script = "SELECT count(*) as cnt FROM quiz_history";
         ResultSet resultSet = null;
         int result = 0;
 
